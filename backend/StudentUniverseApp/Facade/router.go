5747166ed289b20f72cbab76fc169e2a381339a2
garvitgupta97 <garvit.gupta@ufl.edu>
2022-02-24 19:34:13 -0500

    fixes

--- backend/StudentUniverseApp/Facade/router.go
+++ backend/StudentUniverseApp/Facade/router.go
@@ -13,6 +13,7 @@ func setRouter() *gin.Engine {
 	api := router.Group("/api")
 	{
 		api.POST("/signup", signUp)
+		api.OPTIONS("/signup", preflight)
 		api.POST("/signin", signIn)
 		api.POST("/getUsers", getUsers)
 	}
@@ -20,4 +21,10 @@ func setRouter() *gin.Engine {
 	router.NoRoute(func(ctx *gin.Context) { ctx.JSON(http.StatusNotFound, gin.H{}) })
 
 	return router
+}
+
+func preflight(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers")
+	c.JSON(http.StatusOK, struct{}{})
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,55 @@
-
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Login from "./login/login";
-import SignUp from "./signup/signup";
-
-
-import Home from './Home/Home'
-import AboutUs from './AboutUs/AboutUs'
-import Account from './Account/Account'
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ChakraProvider, Box, Flex } from '@chakra-ui/react';
+import theme from './theme';
+import PublicRoute from './components/PublicRoute';
+import PrivateRoute from './components/PrivateRoute';
+import Navbar from './components/Navbar';
+import CommentsPage from './components/CommentsPage';
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
+import CreatePostPage from './components/CreatePostPage';
+import PostList from './components/PostList';
+import ThemedBox from './components/ThemedBox';
+import CreateSubredditPage from './components/CreateSubredditPage';
 
 function App() {
-  return (<Router>
-    <div className="App">
-   
-
-          <Switch>
-            <Route exact path='/' component={Login} />
-            <Route path="/sign-in" component={Login} />
-            <Route path="/sign-up" component={SignUp} />
-            <Route path={'/AboutUs'} component={AboutUs}></Route>
-            <Route path={'/Account'} component={Account}></Route>
-            <Route path={'/home'} component={Home}></Route>
-          </Switch>
-    
-    </div>
-
-
- </Router>
+  return (
+    <ChakraProvider theme={theme}>
+      <Router>
+        <ThemedBox minHeight="100vh" light="gray.300" dark="red.800">
+          <Navbar />
+          <Flex justifyContent="center">
+            <Box width={['95%', '80%', '70%', '60%']} mb={10}>
+              <Switch>
+                <Route path="/r/:subreddit/comments/:id">
+                  <CommentsPage />
+                </Route>
+                <PublicRoute path="/login">
+                  <LoginPage />
+                </PublicRoute>
+                <PublicRoute path="/register">
+                  <RegisterPage />
+                </PublicRoute>
+                <PrivateRoute path="/submit">
+                  <CreatePostPage />
+                </PrivateRoute>
+                <PrivateRoute path="/subreddits/create">
+                  <CreateSubredditPage />
+                </PrivateRoute>
+                <Route path="/r/:subreddit">
+                  <PostList />
+                </Route>
+                <Route path="/">
+                  <PostList />
+                </Route>
+              </Switch>
+            </Box>
+          </Flex>
+        </ThemedBox>
+      </Router>
+    </ChakraProvider>
   );
 }
-
 
 export default App;
